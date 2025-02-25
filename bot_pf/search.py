db8085254683eb1ca851a5707434c0b3fc0b3ce1
5yichan <snusae@gmail.com>
2025-02-25 14:28:54 +0800

    add params 'start' = 0

--- bot_pf/search.py
+++ bot_pf/search.py
@@ -27,6 +27,7 @@ def search_image_with_google(query: str) -> str:
         "cx": CSE_ID,
         "q": query,  # 搜尋的關鍵字
         "searchType": "image",  # 指定搜尋圖片
+        "start": 0,
         "num": 5,  # 回傳 5 張圖片
     }
     first_image_url = None
